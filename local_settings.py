4ea0486f2ef097ec9fbb36ec568da962b368c1b0
mathhomework <toxstalks@gmail.com>
2014-07-10 11:06:46 -0700

    fresh project

---
+++ local_settings.py
@@ -0,0 +1,10 @@
+__author__ = 'Andrew'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'story',
+        'USER': 'postgres',
+        'PASSWORD': 'hotdog'
+    }
+}
